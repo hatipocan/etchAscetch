a936782bab42bc601019c81c0c8d95e0322ab50b
hatipocan <97190685+hatipocan@users.noreply.github.com>
2023-05-04 09:19:43 +0300

    reset button activated

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
-// this function adds a div to the frame container 
+// this function adds a div to the frame container.
+// and change the color of the tile with mouse over.
 
 function addGridPiece (num) {
     for (let i = num*num; i > 0 ; i-- ) {
@@ -16,7 +17,26 @@ function addGridPiece (num) {
 
 }
 
-addGridPiece(40);
+// starter prompt for the web page with the user imput. which fires the function accordingly.
+
+let welcomeMessage = Number(prompt('Hello welcome to the scetch app. Please enter a number between 1-40'));
+
+if (welcomeMessage > 40) {
+    alert('The number is too big!!!');
+    location.reload();
+}
+
+
+addGridPiece(welcomeMessage);
+
+// a restart condition if the button is pressed.
+
+const btn = document.getElementById('reset-button');
+btn.addEventListener('click', () => {
+    location.reload()
+});
+
+
 
 
 
